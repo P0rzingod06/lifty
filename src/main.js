d9216617e7355a59a3beb383a1e4c727496f99eb
Michael Gassert <gassertm2243@gmail.com>
2018-04-29 10:39:47 -0500

    Set up application template with Vuetify

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import App from './App'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuetify)
 
 /* eslint-disable no-new */
 new Vue({
